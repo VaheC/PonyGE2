5c46c94f68b9362a6b3a8a55406afbec29ec4e56
VaheC <vcharchian@gmail.com>
2024-10-09 21:32:44 +0400

    btc all for all data selected

--- src/fitness/max_fitness_numba.py
+++ src/fitness/max_fitness_numba.py
@@ -12,21 +12,26 @@ import os
 def generate_data():
     # df = pd.read_csv(Path(r'C:/\Users/\vchar/\OneDrive/\Desktop/\ML Projects/\Upwork/\AlgoT_ML_Dev/\GrammarEvolution/\PonyGE2/\datasets/\BTCUSD_ohlcv.csv'))
     # df = pd.read_csv(Path(r'C:/\Users/\vchar/\OneDrive/\Desktop/\ML Projects/\Upwork/\AlgoT_ML_Dev/\GrammarEvolution/\PonyGE2/\datasets/\BTC-ETH-1m.csv'))
-    # df = pd.read_csv(Path(r'C:/\Users/\vchar/\OneDrive/\Desktop/\ML Projects/\Upwork/\AlgoT_ML_Dev/\GrammarEvolution/\PonyGE2/\all_data_1min.csv'))
-    df = pd.read_csv('/kaggle/input/btcusd-test/BTCUSD_ohlcv.csv')
+    # df = pd.read_csv(Path(r'C:/\Users/\vchar/\OneDrive/\Desktop/\ML Projects/\Upwork/\AlgoT_ML_Dev/\GrammarEvolution/\PonyGE2/\datasets/\all_data_1min.csv'))
+    # df = pd.read_csv('/kaggle/input/btcusd-test/BTCUSD_ohlcv.csv')
     # df = pd.read_csv('/kaggle/input/btcusd-test/BTC-ETH-1m.csv')
-    # df = pd.read_csv('/kaggle/input/btcusd-test/all_data_1min.csv')
+    df = pd.read_csv('/kaggle/input/btcusd-test/all_data_1min.csv')
     # df['datetime'] = pd.to_datetime(df['datetime'])
     # df = df.iloc[-10080:]
     # df = df.iloc[-525600:]
     df.sort_values('datetime', ascending=True, inplace=True)
     df.reset_index(inplace=True, drop=True)
     price_data = {}
-    price_data['open'] = df['open'].values
-    price_data['close'] = df['close'].values
-    price_data['high'] = df['high'].values
-    price_data['low'] = df['low'].values
-    price_data['volume'] = df['volume'].values
+    # price_data['open'] = df['open'].values
+    # price_data['close'] = df['close'].values
+    # price_data['high'] = df['high'].values
+    # price_data['low'] = df['low'].values
+    # price_data['volume'] = df['volume'].values
+    for col in df.columns:
+        if col == 'datetime':
+            continue
+        else:
+            price_data[col] = df[col].values
     return price_data
 
 class max_fitness_numba(base_ff):
