82ea6180afb88777f7ba50b470204732cf1365f4
VaheC <vcharchian@gmail.com>
2024-11-25 15:52:43 +0400

    tpu removed

--- src/ponyge_kaggle.py
+++ src/ponyge_kaggle.py
@@ -15,35 +15,6 @@ from stats.stats import get_stats
 from algorithm.parameters import params, set_params
 import sys
 
-import tensorflow as tf
-
-def use_tpu():
-    # Check if TPU is available and connect
-    try:
-        # Initialize TPU
-        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  
-        print('Running on TPU ', tpu.cluster_spec().as_dict()['worker'])
-        
-        # Connect to TPU and initialize the system
-        tf.config.experimental_connect_to_cluster(tpu)
-        tf.tpu.experimental.initialize_tpu_system(tpu)
-        
-        # Create a distribution strategy for TPU
-        strategy = tf.distribute.TPUStrategy(tpu)
-    except ValueError:
-        print('No TPU found. Falling back to CPU or GPU.')
-        strategy = tf.distribute.get_strategy()  # Use CPU or GPU fallback
-
-    # Use the TPU strategy scope to execute code on the TPU
-    with strategy.scope():
-        # Create two random tensors
-        tensor_a = tf.random.uniform(shape=[3, 3], minval=0, maxval=10, dtype=tf.float32)
-        tensor_b = tf.random.uniform(shape=[3, 3], minval=0, maxval=10, dtype=tf.float32)
-        
-        # Add the two tensors
-        result = tf.add(tensor_a, tensor_b)
-
-
 def mane():
     """ Run program """
     set_params(sys.argv[1:])  # exclude the ponyge.py arg itself
@@ -57,4 +28,3 @@ def mane():
 
 if __name__ == "__main__":
     mane()
-    use_tpu()
