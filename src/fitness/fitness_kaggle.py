8fe0bf64e83d0085e82ede9918e990e428467c07
VaheC <vcharchian@gmail.com>
2024-12-23 10:27:16 +0400

    fold1 30min as  a source

--- src/fitness/fitness_kaggle.py
+++ src/fitness/fitness_kaggle.py
@@ -10,8 +10,9 @@ from pathlib import Path
 
 def generate_data():
 
-    df = pd.read_csv('/kaggle/input/btcusd-test/data_folds_60min/data_fold3.csv')
+    # df = pd.read_csv('/kaggle/input/btcusd-test/data_folds_60min/data_fold3.csv')
     # df = pd.read_csv('/kaggle/input/btcusd-test/data_60min_train.csv')
+    df = pd.read_csv('/kaggle/input/btcusd-test/data_folds_30min/data_fold1.csv')
     df['datetime'] = pd.to_datetime(df['datetime'])
     df.sort_values('datetime', ascending=True, inplace=True)
     df.reset_index(inplace=True, drop=True)
