c36faed7d90db450e745dd659c63530d453f69fd
VaheC <vcharchian@gmail.com>
2025-01-30 07:18:14 +0400

    not important

--- src/fitness/performance/filter_strategies2.py
+++ src/fitness/performance/filter_strategies2.py
@@ -69,9 +69,10 @@ def generate_strategy_data(strategy_file_path):
 
     # df_str = df_str[df_str['fitness'] < -1].reset_index(drop=True)#.iloc[:10000]
     # df_str = df_str[(df_str['fitness'] < -1) & (df_str['fitness'] >= -5)].reset_index(drop=True)#.iloc[:10000]
+    # df_str = df_str[(df_str['fitness'] < -5) & (df_str['fitness'] >= -30)].reset_index(drop=True)#.iloc[:10000]
     # df_str = df_str[(df_str['fitness'] < -50) & (df_str['fitness'] >= -100)].reset_index(drop=True)#.iloc[:10000]
-    df_str = df_str[(df_str['fitness'] < -100) & (df_str['fitness'] >= -1000)].reset_index(drop=True)#.iloc[:10000]
-    # df_str = df_str[(df_str['fitness'] < -1000)].reset_index(drop=True)#.iloc[:10000]
+    # df_str = df_str[(df_str['fitness'] < -100) & (df_str['fitness'] >= -1000)].reset_index(drop=True)#.iloc[:10000]
+    df_str = df_str[(df_str['fitness'] < -100)].reset_index(drop=True)#.iloc[:10000]
 
     df_str.sort_values('fitness', ascending=True, inplace=True)
     df_str.reset_index(drop=True, inplace=True)
